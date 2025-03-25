ad11a26c72d7d57aaf58642c7e3aa43cfc775713
Mike Laurel <mlaurel@hey.com>
2025-03-25 09:19:26 -0700

    Adds logger and initialize in main.go

--- main.go
+++ main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"mlaurel.dev/movies/logger"
 )
 
 func main() {
+	// Initialize logger
+	logInstance := initializeLogger()
+
 	// Serve static files
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
 	// Start server
 	const addr = ":8080"
+	logInstance.Info("Server starting on " + addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
+		logInstance.Error("Server failed to start", err)
 		log.Fatalf("Server failed: %v", err)
 	}
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to Initialize logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
 }
